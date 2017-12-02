7cb5ca1a8d003d739a4e66ce6e4a48d74398a455
joshua-murphy <josh.murphy11@gmail.com>
2017-12-01 21:46:30 -0700

    addresses controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
 
   root "trips#index"
   resources :trips do
-  resources :locations
+    resources :locations
+  end
+
+  scope "locations/:location_id", as: "location" do
+    resources :addresses
   end
 
 end
